7ee8e0580e4fe3ec7e68bd579e9834df15af8699
alu0100882186 <alu0100882186@ull.edu.es>
2017-03-03 21:43:58 +0000

    subiendo pagina

--- src/router_all.js
+++
@@ -1,9 +0,0 @@
-var express = require('express')
-var app = express()
-
-router.all('*', requireAuthentication)
-router.all('*', loadUser);
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
